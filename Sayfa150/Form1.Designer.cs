9965c755028c37e8cee588ba75183975f400a5f4
berat584 <berat584@BPC08>
2024-03-19 10:03:19 +0300

    Proje dosyası ekle.

---
+++ Sayfa150/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Sayfa150
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ad = new System.Windows.Forms.TextBox();
+            this.numara = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ogrenciler = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 30);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 30);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 191);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 30);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ad
+            // 
+            this.ad.Location = new System.Drawing.Point(215, 52);
+            this.ad.Name = "ad";
+            this.ad.Size = new System.Drawing.Size(126, 20);
+            this.ad.TabIndex = 1;
+            // 
+            // numara
+            // 
+            this.numara.Location = new System.Drawing.Point(241, 78);
+            this.numara.Name = "numara";
+            this.numara.Size = new System.Drawing.Size(100, 20);
+            this.numara.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adınızı girin";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numaranızı girin";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Ogrenciler
+            // 
+            this.Ogrenciler.FormattingEnabled = true;
+            this.Ogrenciler.Location = new System.Drawing.Point(433, 35);
+            this.Ogrenciler.Name = "Ogrenciler";
+            this.Ogrenciler.Size = new System.Drawing.Size(160, 186);
+            this.Ogrenciler.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Ogrenciler);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numara);
+            this.Controls.Add(this.ad);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox ad;
+        private System.Windows.Forms.TextBox numara;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox Ogrenciler;
+    }
+}
+
---
+++ Sayfa150/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayfa150
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<int, string> ogrenciler = new Dictionary<int, string>();
+        int anahtar;
+        string deger;
+        private void Listele()
+        {
+            Ogrenciler.Items.Clear();
+            foreach (var ogrenci in ogrenciler)
+            {
+                Ogrenciler.Items.Add(ogrenci.Key +"-"+ogrenci.Value);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            anahtar = int.Parse(numara.Text);
+            deger = ad.Text;
+            ogrenciler.Add(anahtar, deger);
+            Listele();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            anahtar = int.Parse(numara.Text);
+            deger = ad.Text;
+            ogrenciler[anahtar] = deger;
+            Listele();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            anahtar = int.Parse(numara.Text);
+            ogrenciler.Remove(anahtar);
+            Listele();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool durum = false;
+            if (numara.Text !="")
+            {
+                anahtar = int.Parse(numara.Text);
+                durum = ogrenciler.ContainsKey(anahtar);
+            }
+            else
+            {
+                deger = ad.Text;
+                durum = ogrenciler.ContainsValue(deger);
+            }
+            if (durum == true)
+            {
+                MessageBox.Show("Öğrenci Kayıtlıdır.");
+            }
+            else
+            {
+                MessageBox.Show("Öğrenci Kayıtlı Değildir.");
+            }
+        }
+    }
+}
